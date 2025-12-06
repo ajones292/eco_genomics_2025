8970d313a43f7d091f7cd92c2e6ab44c592f3210
Aidan Tristan Jones <ajones76@hc-node02.cluster>
2025-12-06 00:45:11 -0500

    Final project docs and scripts

---
+++ final_project/myscripts/download.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+#---------  Slurm preamble, defines the job with #SBATCH statements
+
+# Give your job a name that's meaningful to you, but keep it short
+#SBATCH --job-name=process_stats
+
+# Name the output file: Re-direct the log file to your home directory
+# The first part of the name (%x) will be whatever you name your job 
+#SBATCH --output=/users/a/j/ajones76/projects/eco_genomics_2025/final_project/mylogs/%x_%j.out
+
+# Which partition to use: options include short (<3 hrs), general (<48 hrs), or week
+#SBATCH --partition=general
+
+# Specify when Slurm should send you e-mail.  You may choose from
+# BEGIN, END, FAIL to receive mail, or NONE to skip mail entirely.
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=ajones76@uvm.edu
+
+# Run on a single node with four cpus/cores and 8 GB memory
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=20
+#SBATCH --mem=64G
+
+# Time limit is expressed as days-hrs:min:sec; this is for 24 hours.
+#SBATCH --time=24:00:00
+
+#---------  End Slurm preamble, job commands now follow
+
+cd /users/a/j/ajones76/projects/eco_genomics_2025/final_project/mydata
+
+wget https://www.dfam.org/releases/current/families/FamDB/dfam39_full.15.h5.gz
